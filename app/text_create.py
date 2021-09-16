1f4d2966d0a658a9d766c80498afe59a59664d1c
Daniel <dpook@spartaglobal.com>
2021-09-16 10:40:03 +0100

    Need to add data base

--- app/text_create.py
+++ app/text_create.py
@@ -4,8 +4,7 @@ class File_writer:
 
     def open_file(name):
         try:
-            with open(f"Password_storage/{name}.txt", "a+") as file:
-                file.write(f"This is a storage of tested passwords for: {name} \n")
+            with open(f"Password_storage/{name}.txt", "a") as file:
                 file.close()
         except Exception as e:
             print(e)
@@ -15,7 +14,7 @@ class File_writer:
 
     def write_file(name,sentence):
         try:
-            f = open(f"Password_storage/{name}", "a")
+            f = open(f"Password_storage/{name}.txt", "a")
             f.write(sentence)
 
             f.close()
@@ -32,7 +31,8 @@ class File_writer:
                 File_writer.write_file(name,f"The password checked was {password} \n")
                 for item in s_array:
                     File_writer.write_file(name,item)
-                File_writer.write_file(name,"\n \n")
+                    File_writer.write_file(name,"\n")
+                File_writer.write_file(name,"\n ----------------------------------------------------------------------------------------------- \n")
             elif answ == 'No':
                 loop_token = False
                 return
