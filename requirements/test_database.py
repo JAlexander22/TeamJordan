90d1af9329972967b3f461f96935bcaa2a7bf50f
mohammed <mali@spartaglobal.com>
2021-09-16 10:27:28 +0100

    created database for common passwords and included a function to check our password against it

--- requirements/test_database.py
+++ requirements/test_database.py
@@ -7,36 +7,40 @@ def create_db(conn, table_name):
                     ;''')
     print("Table created successfully")
 
-def interate_pass(conn):
-  file = open('passwords.txt','r')
+def interate_pass(conn, table_name):
+  file = open('common_passwords.txt','r')
   data = file.readlines()
 
   for common in data:
-      print(common)
-      command = f"INSERT INTO company (COMMONPASSWORDS) VALUES ('{common}');"
+      #print(common)         # Keep this commented, unless you want to see all 10,000 values printed individually on the terminal
+      command = f"INSERT INTO {table_name} (COMMONPASSWORDS) VALUES ('{common}');"
       conn.commit()
       conn.execute(command)
   return data
 
 def get_values(conn):
-    query = "SELECT * FROM company;"
+    query = "SELECT * FROM common_passwords;"
     conn.commit()
     result = conn.execute(query).fetchall()
     print(result)
 
-def main():
-    conn = sqlite3.connect('test.db')
-    print ("Opened database nicely")
-    create_db(conn, 'company')
-    # conn.close()
-    results = interate_pass(conn)
-    print('\nGetting values from DB:')
-    get_values(conn)
+def check_password_in_db(conn):
 
-    # command = f"INSERT INTO company (COMMONPASSWORDS) VALUES ('{results}');"
-    # conn.commit()
-    # conn.execute(command)
-    # print("records created successfully")
+    if conn.execute("SELECT 1 FROM common_passwords WHERE COMMONPASSWORDS = '123456\n'").fetchone():
+        print("Found!")
+    else:
+        print("Not Found!")
+
+def main():
+    conn = sqlite3.connect('common_passwords.db')
+    print ("Opened database nicely")
+    #create_db(conn, 'common_passwords')                       # 1. comment this after creatiing the database for the first time
+    # conn.close()
+    #results = interate_pass(conn, 'common_passwords')         # 2. comment this after creatiing the database for the first time
+    #print('\nGetting values from DB:')                        # 3.1 uncomment this if you want to see the values from a table in the database
+    #get_values(conn)                                          # 3.2 uncomment this if you want to see the values from a table in the database
+    print("checking if password created is in commoon password database:")
+    check_password_in_db(conn)
     conn.close()
 
 if __name__ == '__main__':
